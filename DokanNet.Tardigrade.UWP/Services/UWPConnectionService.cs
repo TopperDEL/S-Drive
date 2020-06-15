ff3adda1f7ae44d01bc1eeb75af97a316fe796b4
TopperDEL <github_com@tparth.de>
2020-06-15 15:32:55 +0200

    fix: Catch Mount/Unmount-errors

--- DokanNet.Tardigrade.UWP/Services/UWPConnectionService.cs
+++ DokanNet.Tardigrade.UWP/Services/UWPConnectionService.cs
@@ -20,33 +20,51 @@ namespace DokanNet.Tardigrade.UWP.Services
 
         public async Task<bool> SendMountAllAsync(List<MountParameters> mounts)
         {
-            var message = new ValueSet();
-            message.Add(Messages.MountAll, Newtonsoft.Json.JsonConvert.SerializeObject(mounts));
-            var result = await _connection.SendMessageAsync(message);
-            return result.Status == AppServiceResponseStatus.Success;
+            try
+            {
+                var message = new ValueSet();
+                message.Add(Messages.MountAll, Newtonsoft.Json.JsonConvert.SerializeObject(mounts));
+                var result = await _connection.SendMessageAsync(message);
+                return result.Status == AppServiceResponseStatus.Success;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public async Task<bool> SendUnmountAllAsync()
         {
-            var message = new ValueSet();
-            message.Add(Messages.UnmountAll, "");
-            var result = await _connection.SendMessageAsync(message);
-            return result.Status == AppServiceResponseStatus.Success;
+            try
+            {
+                var message = new ValueSet();
+                message.Add(Messages.UnmountAll, "");
+                var result = await _connection.SendMessageAsync(message);
+                return result.Status == AppServiceResponseStatus.Success;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public async Task<bool> GetAreDrivesMounted()
         {
-            var message = new ValueSet();
-            message.Add(Messages.AreDrivesMounted, "inhalt");
-            var result = await _connection.SendMessageAsync(message);
-            if (result.Status == AppServiceResponseStatus.Success)
+            try
             {
-                var response = result.Message.FirstOrDefault();
-                if (response.Key == Messages.AreDrivesMountedResult)
+                var message = new ValueSet();
+                message.Add(Messages.AreDrivesMounted, "inhalt");
+                var result = await _connection.SendMessageAsync(message);
+                if (result.Status == AppServiceResponseStatus.Success)
                 {
-                    return (bool)response.Value;
+                    var response = result.Message.FirstOrDefault();
+                    if (response.Key == Messages.AreDrivesMountedResult)
+                    {
+                        return (bool)response.Value;
+                    }
                 }
             }
+            catch { }
             return false;
         }
     }
