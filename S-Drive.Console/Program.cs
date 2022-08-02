9ba835ef8512a21a19ec128e21b9e18998f3c804
TopperDEL <github_com@tparth.de>
2022-08-02 10:17:01 +0200

    Update Program.cs

--- S-Drive.Console/Program.cs
+++ S-Drive.Console/Program.cs
@@ -1,4 +1,4 @@
-﻿using S_Drive.Contracts.Models;
+﻿using NC.DokanFS;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,20 +11,24 @@ namespace S_Drive.Console
     {
         static void Main(string[] args)
         {
-            StorjMount storjMount = new StorjMount();
+            var storjDisk = new StorjDisk(new uplink.NET.Models.Access(""), "s-drive");
+            var dokan = new DokanFrontend(storjDisk, "Storj");
 
-            MountParameters mountParameters = new MountParameters();
-            mountParameters.DriveLetter = DriveLetters.s;
+            //MountParameters mountParameters = new MountParameters();
+            //mountParameters.DriveLetter = DriveLetters.s;
 
-            //Via Access-Grant:
-            
+            ////Via Access-Grant:
+            //mountParameters.Bucketname = "s-drive";
+
             //Via Satellite, API key and Passphrase:
             //mountParameters.SatelliteAddress = args[0];
             //mountParameters.ApiKey = args[1];
             //mountParameters.EncryptionPassphrase = args[2];
             //mountParameters.Bucketname = args[3];
-
-            storjMount.MountAsync(mountParameters).Wait();
+            MountParameters mountParameters = new MountParameters();
+            mountParameters.DriveLetter = DriveLetters.s;
+            mountParameters.VolumeLabel = "S-Drive";
+            storjDisk.MountAsync(mountParameters, dokan).Wait();
         }
     }
 }
