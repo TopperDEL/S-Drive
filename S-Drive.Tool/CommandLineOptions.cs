7c97c397cde8069d03032303df583aa985348dae
TopperDEL <github_com@tparth.de>
2022-08-02 14:02:16 +0200

    feat: Include command line parser for Tool

---
+++ S-Drive.Tool/CommandLineOptions.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_Drive.Tool
+{
+    public class CommandLineOptions
+    {
+        [Option('g',"access-grant", Required = true, HelpText = "The access grant on Storj DCS, that should be used.")]
+        public string AccessGrant { get; set; }
+
+        [Option('b', "bucket-name", Required = true, HelpText = "The name of the bucket that should be used.")]
+        public string BucketName { get; set; }
+
+        [Option('l', "drive-label", Required = true, HelpText = "The name of the drive to create - will appear as name in the Windows Explorer.")]
+        public string DriveLabel { get; set; }
+
+        [Option('d', "drive-letter", Required = true, HelpText = "The drive letter of the hard drive to use - must not be in use already.")]
+        public string DriveLetter { get; set; }
+    }
+}
---
+++ S-Drive.Tool/Program.cs
@@ -0,0 +1,39 @@
+﻿using CommandLine;
+using NC.DokanFS;
+using S_Drive;
+using S_Drive.Tool;
+using System.Reflection;
+
+Parser.Default.ParseArguments<CommandLineOptions>(args)
+                  .WithParsed<CommandLineOptions>(o =>
+                  {
+                      var assembly = Assembly.GetExecutingAssembly();
+                      var assemblyVersion = assembly.GetName().Version;
+                      Console.WriteLine($"Welcome to S-Drive v{assemblyVersion.Major}.{assemblyVersion.Minor}.{assemblyVersion.Build}!");
+                      Console.WriteLine("Checking for Dokany 2...");
+
+                      if (!System.IO.File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "dokan2.dll")))
+                      {
+                          Console.WriteLine("Dokany is not installed - please install Dokany 2 from here:");
+                          if (Environment.Is64BitProcess)
+                              Console.WriteLine("https://github.com/dokan-dev/dokany/releases/download/v2.0.5.1000/Dokan_x64.msi");
+                          else
+                              Console.WriteLine("https://github.com/dokan-dev/dokany/releases/download/v2.0.5.1000/Dokan_x86.msi");
+
+                          return;
+                      }
+                      else
+                      {
+                          Console.WriteLine("Dokany is installed!");
+                      }
+
+                      var storjDisk = new StorjDisk(new uplink.NET.Models.Access(o.AccessGrant), o.BucketName);
+                      var dokan = new DokanFrontend(storjDisk, "Storj");
+
+                      var driveLetter = (DriveLetters)Enum.Parse(typeof(DriveLetters), o.DriveLetter.ToLower());
+                      MountParameters mountParameters = new MountParameters();
+                      mountParameters.DriveLetter = driveLetter;
+                      mountParameters.VolumeLabel = o.DriveLabel;
+                      Console.WriteLine($"Starting drive '{o.DriveLetter}' with label '{o.DriveLabel}'...");
+                      storjDisk.MountAsync(mountParameters, dokan).Wait();
+                  });
